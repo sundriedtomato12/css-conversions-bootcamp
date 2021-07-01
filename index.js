5d3a99207384416335e063627f3566938ac36042
michelle-mok <michellemokhuihui@gmail.com>
2021-07-01 13:25:43 +0800

    first commit base solution

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const hexCode = process.argv[2];
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+// hexCode is process.argv[2], which is the user's input
+console.log('converting hex to rgb...');
+console.log(hexToRgb(hexCode));
